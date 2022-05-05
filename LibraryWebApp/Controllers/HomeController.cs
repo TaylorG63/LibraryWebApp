70ace2f14e27a13c36ef4b04e1fd9bb2a0e66c35
Taylor G <taylorg09784@gmail.com>
2022-05-05 12:57:56 -0500

    Added Login and Register Views
    Attempted to get Login and Register into a working state, will focus on UI for now and leave functionality for later.

--- LibraryWebApp/Controllers/HomeController.cs
+++ LibraryWebApp/Controllers/HomeController.cs
@@ -23,6 +23,34 @@ namespace LibraryWebApp.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel model )
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterModel register)
+        {
+            if (ModelState.IsValid)
+            {
+                //ToDo - finish code
+                //process and register the user
+                //insert user into the database
+            }
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
